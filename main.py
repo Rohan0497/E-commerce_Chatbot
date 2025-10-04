5c1fbe8d51baf5171513593a4ab898f196d3677e
Rohan0497 <78637250+Rohan0497@users.noreply.github.com>
2025-10-04 19:59:18 +0100

    faq route implemented

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 import streamlit as st
+from app.router import router 
+from app.faq import ingest_faq_data, faq_chain  
+from pathlib import Path
+
+faq_path = Path(__file__).parent / "faq_data.csv"
+ingest_faq_data(faq_path)
+
+def ask(query):
+    route = router(query).name
+    if route == "faq":
+        return faq_chain(query)
+    else:
+        return f"Route {route} not implemented yet"
 
 st.title("E-commerce Bot")
 query = st.chat_input("Write your query")
@@ -18,7 +31,8 @@ if query:
         st.markdown(query)
     st.session_state.messages.append({"role":"user", "content":query})
 
-    response = "I am good"
+    # response = "I am good"
+    response = ask(query)
     with st.chat_message("assistant"):
         st.markdown(response)
     st.session_state.messages.append({"role": "assistant", "content": response})
